c9d02b8dbef8f5386789037cad6b7b7d4a2b1ae4
MariamWafik349 <mariamwafik10@gmail.com>
2024-12-18 23:38:16 +0200

    Create ymal.groovy

---
+++ CI_CD with Jenkins/lab24/ymal.groovy
@@ -0,0 +1,3 @@
+def call(String deploymentFile, String registry, String username, String imageName, String tag) {
+    sh "sed -i 's|image:.*|image: ${registry}/${username}/${imageName}:${tag}|g' ${deploymentFile}"
+}
