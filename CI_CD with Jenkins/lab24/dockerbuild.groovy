2a74422a1c20fcc6425737c5d6d73633fdb0b08a
MariamWafik349 <mariamwafik10@gmail.com>
2024-12-18 23:36:57 +0200

    Create dockerbuild.groovy

---
+++ CI_CD with Jenkins/lab24/dockerbuild.groovy
@@ -0,0 +1,3 @@
+def call(String dockerfile, String imageName, String tag, String registry, String username, String password) {
+    sh "docker build -f ${dockerfile} -t ${registry}/${username}/${imageName}:${tag} ."
+}
