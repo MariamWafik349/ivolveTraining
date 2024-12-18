e5617a27d9045aa2c478ab892c276c4dd4dacf97
MariamWafik349 <mariamwafik10@gmail.com>
2024-12-18 23:37:27 +0200

    Create dockerpush.groovy

---
+++ CI_CD with Jenkins/lab24/dockerpush.groovy
@@ -0,0 +1,6 @@
+def call(String imageName, String tag, String registry, String username, String password) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
+    }
+    sh "docker push ${registry}/${username}/${imageName}:${tag}"
+}
